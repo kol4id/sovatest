ba04d4a437d9169db7da98ff6cc3d3767b1202a8
Kirill Nagulin <over90006at9@gmail.com>
2025-04-30 23:24:07 +0300

    Implement Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
+import { useAppDispatch } from "../store/store";
+import { fetchProducts } from "../store/productsSlice";
 
 
 
 const Home = () =>{
+    const dispatch = useAppDispatch();
+
+    useEffect(()=>{
+        dispatch(fetchProducts())
+    },[])
+
     return(
         <>
-        <div>TEST</div>
+            <div>TEST</div>
         </>
     )
 }
