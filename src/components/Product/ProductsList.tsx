992c82d6151969e7f19e34bc35a42854b9f9a860
Kirill Nagulin <over90006at9@gmail.com>
2025-05-01 23:10:20 +0300

    Implement pagination in ProductsArticle and ProductsList components

--- src/components/Product/ProductsList.tsx
+++ src/components/Product/ProductsList.tsx
@@ -2,15 +2,23 @@ import { useSelector } from "react-redux"
 import { RootState } from "../../store/store"
 import ProductCard from "./ProductCard"
 import styles from "./ProductList.module.scss"
+import { FC, useMemo } from "react"
+import { PAGE_LIMIT } from "../../config"
 
-const ProductsList = () => {
+
+interface IProps{
+    page?: number
+}
+const ProductsList: FC<IProps> = ({page = 1}) => {
 
     const products = useSelector((state: RootState) => state.products.products)
+
+    const productsByPage = useMemo(() => products.slice((page - 1) * PAGE_LIMIT, page * PAGE_LIMIT), [products, page]); 
 
     return (
         <>
             <ul className={styles.product_container}>
-                {products.map((product) => (
+                {productsByPage.map((product) => (
                     <li key={product.id}>
                         <ProductCard product={product}/>
                     </li>
