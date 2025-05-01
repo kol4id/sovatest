5d854d5c8915342fa96ade4883c9d9a6a3cd1103
Kirill Nagulin <over90006at9@gmail.com>
2025-05-01 21:36:14 +0300

    Temporal changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,18 @@
 import { useEffect } from "react";
 import { useAppDispatch } from "../store/store";
 import { fetchProducts } from "../store/productsSlice";
-
-
+import ProductsArticle from "../components/Product/ProductsArticle";
 
 const Home = () =>{
     const dispatch = useAppDispatch();
 
     useEffect(()=>{
-        dispatch(fetchProducts())
+        dispatch(fetchProducts(100))
     },[])
 
     return(
         <>
-            <div>TEST</div>
+            <ProductsArticle/>
         </>
     )
 }
