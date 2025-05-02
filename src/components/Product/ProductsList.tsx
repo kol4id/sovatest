622d823d31a058fd0bb3f5f3db3a515c9e7ceb11
Kirill Nagulin <over90006at9@gmail.com>
2025-05-02 23:18:28 +0300

    Refactor ProductsList component to use custom hook for product filtering; remove Redux state selection

--- src/components/Product/ProductsList.tsx
+++ src/components/Product/ProductsList.tsx
@@ -1,20 +1,16 @@
-import { useSelector } from "react-redux"
-import { RootState } from "../../store/store"
 import ProductCard from "./ProductCard"
 import styles from "./ProductList.module.scss"
 import { FC, useMemo } from "react"
 import { PAGE_LIMIT } from "../../config"
-
+import useFilterProducts from "../../hooks/useFilterProducts"
 
 interface IProps{
     page?: number
 }
 const ProductsList: FC<IProps> = ({page = 1}) => {
-
-    const products = useSelector((state: RootState) => state.products.products)
-
-    const productsByPage = useMemo(() => products.slice((page - 1) * PAGE_LIMIT, page * PAGE_LIMIT), [products, page]); 
-
+    const filteredProducts = useFilterProducts();
+    const productsByPage = useMemo(() => filteredProducts.slice((page - 1) * PAGE_LIMIT, page * PAGE_LIMIT), [filteredProducts, page]);
+   
     return (
         <>
             <ul className={styles.product_container}>
