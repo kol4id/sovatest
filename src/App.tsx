438cca6f35e3931678a22145d87e8be5d9b22a5e
Kirill Nagulin <over90006at9@gmail.com>
2025-05-02 13:16:29 +0300

    Refactor App and Home components to streamline product fetching logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import { useEffect } from 'react';
+import { useAppDispatch } from './store/store';
+import { fetchProducts } from './store/productsSlice';
 
 function App() {
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts(100)); 
+  }, [dispatch]);
+  
   return (
     <>
       <Outlet />
