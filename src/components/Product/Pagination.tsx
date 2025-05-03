74a4fbb48c79342d2b8e58be6d9e17a2b9f2f0f7
Kirill Nagulin <over90006at9@gmail.com>
2025-05-03 15:34:28 +0300

    Docomented Pagination component; removed Link overhead

--- src/components/Product/Pagination.tsx
+++ src/components/Product/Pagination.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import styles from "./Pagination.module.scss";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../store/store";
 import { setCurrentPage } from "../../store/productSearchSlice";
 
@@ -9,11 +9,21 @@ interface PaginationProps {
     currentPage: number;
 }
 
+/**
+ * Компонент навигации по страницам, при навигации
+ * меняет глобальное состояние currentPage
+ * 
+ * @param totalPages - всего страниц
+ * @param currentPage - текущая страница
+ * @returns компонент с двумя кнопками для навигации
+ */
 const Pagination: FC<PaginationProps> = ({ totalPages, currentPage}) => {
     const dispatch = useAppDispatch();
+    const navigate = useNavigate();
 
     const handlePageChange = (pageModifier: number) => {
         dispatch(setCurrentPage(currentPage + pageModifier));
+        navigate(`/products/${currentPage + pageModifier}`);
     }
 
     if (totalPages <= 1) return null;
@@ -21,21 +31,21 @@ const Pagination: FC<PaginationProps> = ({ totalPages, currentPage}) => {
     return(
         <>
             <nav aria-label="Page navigation" className={styles.pagination_container}>
-                <Link to={`/products/${currentPage - 1}`} style={{ all: 'unset' }}>
-                    <button 
-                        className={styles.page_button}
-                        onClick={() => handlePageChange(-1)}
-                        disabled={currentPage === 1}
-                        style={{marginRight: '10px'}}
-                    >Previous</button>
-                </Link>
-                <Link to={`/products/${currentPage + 1}`} style={{ all: 'unset' }}>
-                    <button
-                        className={styles.page_button}
-                        onClick={() => handlePageChange(1)}
-                        disabled={currentPage === totalPages}
-                    >Next</button>
-                </Link>
+                <button 
+                    className={styles.page_button}
+                    onClick={() => handlePageChange(-1)}
+                    disabled={currentPage === 1}
+                    style={{marginRight: '10px'}}
+                    title="Перейти на предыдущую страницу"
+                    aria-label="Перейти на предыдущую страницу"
+                >Previous</button>
+                <button
+                    className={styles.page_button}
+                    onClick={() => handlePageChange(1)}
+                    disabled={currentPage === totalPages}
+                    title="Перейти на следующую страницу"
+                    aria-label="Перейти на следующую страницу"
+                >Next</button>
             </nav>
         </>
     )
