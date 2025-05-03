c4920caee738612b491807974ca6a76d08198e16
Kirill Nagulin <over90006at9@gmail.com>
2025-05-03 14:44:10 +0300

    Refactor ProductsList and useFilterProducts to manage current page state and improve product filtering logic

--- src/components/Product/ProductsList.tsx
+++ src/components/Product/ProductsList.tsx
@@ -1,16 +1,26 @@
 import ProductCard from "./ProductCard"
 import styles from "./ProductList.module.scss"
-import { FC, useMemo } from "react"
+import { FC, useEffect, useMemo } from "react"
 import { PAGE_LIMIT } from "../../config"
 import useFilterProducts from "../../hooks/useFilterProducts"
+import { useAppDispatch } from "../../store/store"
+import { setCurrentPage } from "../../store/productSearchSlice"
+import { useNavigate } from "react-router-dom"
 
 interface IProps{
     page?: number
 }
 const ProductsList: FC<IProps> = ({page = 1}) => {
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
     const filteredProducts = useFilterProducts();
     const productsByPage = useMemo(() => filteredProducts.slice((page - 1) * PAGE_LIMIT, page * PAGE_LIMIT), [filteredProducts, page]);
    
+    useEffect(()=>{
+        dispatch(setCurrentPage(1));
+        navigate(`/products/1`);
+    },[filteredProducts, dispatch, navigate])
+
     return (
         <>
             <ul className={styles.product_container}>
--- src/hooks/useFilterProducts.ts
+++ src/hooks/useFilterProducts.ts
@@ -1,32 +1,30 @@
-import { useNavigate } from "react-router-dom"
 import { RootState, useAppDispatch } from "../store/store"
 import { useSelector } from "react-redux"
-import { useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import { setFilteredProducts } from "../store/productsSlice"
-import { setCurrentPage } from "../store/productSearchSlice"
 
-const useFilterProducts = () => {
+
+const useFilterProducts = (isDispatch: boolean = true) => {
     const dispatch = useAppDispatch()
-    const navigate = useNavigate()
+
     const products = useSelector((state: RootState) => state.products.products)    
     const searchValue = useSelector((state: RootState) => state.productSearch.value)
 
     const filtered = useMemo(() =>{
         const value = searchValue.toLowerCase();
-        const filteredProducts = products.filter((product) => {
+        return products.filter((product) => {
             return (
                 product.name.toLowerCase().includes(value) ||
                 product.description.toLowerCase().includes(value) ||
                 product.tags.some((tag) => tag.toLowerCase().includes(value))
             )
         })
-        dispatch(setFilteredProducts(filteredProducts));
+    },[searchValue, products])
 
-        if (value === '')  return filteredProducts;
-        dispatch(setCurrentPage(1));
-        navigate(`/products/1`)
-        return filteredProducts;
-    },[searchValue, products, dispatch])
+    useEffect(() => {
+        if (!isDispatch) return
+        dispatch(setFilteredProducts(filtered));
+    }, [filtered, dispatch, isDispatch]);
 
     return filtered
 }
